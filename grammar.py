5930dc5dddf60dd4178635ed1328a26181d6074b
Instein98 <instein98@gmail.com>
2019-06-25 19:49:22 +0800

    support recognizing arbitrary math expression

--- grammar.py
+++ grammar.py
@@ -19,6 +19,11 @@ identifier = Word(alphas+'_', alphanums+'_')
 space = White(min=1)
 dataType = Literal("int")
 expression = Forward()
-expression << (number | unaryOp + expression | expression + binOp + expression)  # must have large parentheses
+_expression = Forward()
+expression << (number + Optional(_expression)
+            | unaryOp + expression + Optional(_expression)
+            | identifier + Optional(_expression)
+            | LParen + expression + RParen + Optional(_expression))  # must have large parentheses
+_expression << Optional(binOp + (number | unaryOp + expression | identifier | LParen + expression + RParen) + _expression)
 statement = Literal("return") + space + expression + semicolon
 function = dataType('returnType') + identifier('fnName') + LParen + RParen + LBrace + statement('statements') + RBrace
