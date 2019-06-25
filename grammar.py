ee8a648f83692a8b5004030894376506573fa151
Instein98 <instein98@gmail.com>
2019-06-25 20:27:33 +0800

    support recognizing arbitrary math expression using factor/term

--- grammar.py
+++ grammar.py
@@ -12,18 +12,24 @@ BinOp ::= "+" | "-" | "*" | "/"
 from pyparsing import *
 
 LBrace, RBrace, LParen, RParen, semicolon = map(Suppress, "{}();")
+ADD, SUB, MUL, DIV = map(Literal, "+-*/")
+AND, OR, EQ, NE, LT, LE, GT, GE = map(Literal, ["&&", "||", "==", "!=", "<", "<=", ">", ">="])
 unaryOp = oneOf("! - ~")  # must have space separator
-binOp = oneOf("+ - * /")
+# binOp = oneOf("+ - * /")
 number = Word(nums)
 identifier = Word(alphas+'_', alphanums+'_')
 space = White(min=1)
 dataType = Literal("int")
+
 expression = Forward()
+factor = Forward()
+factor << (number | identifier | LParen + expression + RParen | unaryOp + factor)
+_term = Forward()
+_term << Optional((MUL | DIV) + factor + _term)
+term = factor + _term
 _expression = Forward()
-expression << (number + Optional(_expression)
-            | unaryOp + expression + Optional(_expression)
-            | identifier + Optional(_expression)
-            | LParen + expression + RParen + Optional(_expression))  # must have large parentheses
-_expression << Optional(binOp + (number | unaryOp + expression | identifier | LParen + expression + RParen) + _expression)
+_expression << Optional((ADD | SUB) + term + _expression)
+expression << term + _expression
+
 statement = Literal("return") + space + expression + semicolon
 function = dataType('returnType') + identifier('fnName') + LParen + RParen + LBrace + statement('statements') + RBrace
