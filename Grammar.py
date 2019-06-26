7e3508107b3d92afca9ca9c38472eb9709167927
Instein98 <instein98@gmail.com>
2019-06-26 21:09:00 +0800

    Support arbitrary expressions (arithmetic, comparison and logic expressions)

--- Grammar.py
+++ Grammar.py
@@ -11,22 +11,50 @@ BinOp ::= "+" | "-" | "*" | "/"
 
 from pyparsing import *
 
+exprStack = []
+
+def pushFirst(strg, loc, toks):
+    exprStack.append(toks[0])
+
+def pushUnary(strg, loc, toks):
+    for t in toks:
+        if t == '-':
+            exprStack.append('unary -')
+        elif t == '!':
+            exprStack.append('unary !')
+        elif t == '~':
+            exprStack.append('unary ~')
+        else:
+            break
+
+def parseArithExp(exp):
+    global exprStack
+    exprStack[:] = []
+    try:
+        arithmeticExp.parseString(exp, parseAll=True)
+    except ParseException as pe:
+        print(exp, "Parsing Failed:", str(pe))
+    except Exception as e:
+        print(exp, "Failed:", str(e))
+    else:
+        return exprStack
+
 LBrace, RBrace, LParen, RParen, semicolon = map(Suppress, "{}();")
 ADD, SUB, MUL, DIV = map(Literal, "+-*/")
 AND, OR, EQ, NE, LT, LE, GT, GE = map(Literal, ["&&", "||", "==", "!=", "<", "<=", ">", ">="])
 unaryOp = oneOf("! - ~")  # must have space separator
-number = Word(nums+'.')
+number = Regex(r"[+-]?\d+(?:\.\d*)?(?:[eE][+-]?\d+)?")
 identifier = Word(alphas+'_', alphanums+'_')
 space = White(min=1)
 dataType = Literal("int")
 
 # Arithmetic expressions
 expression = Forward()
-
-factor = Forward()
-factor << (number | identifier | LParen + expression + RParen | unaryOp + factor)
-term = factor + ZeroOrMore((MUL | DIV) + factor)
-arithmeticExp = term + ZeroOrMore((ADD | SUB) + term)
+atom = ((0,None)*unaryOp + (Group(LParen + expression + RParen)
+    | (number | identifier + LParen + expression + RParen | identifier ).setParseAction( pushFirst ))).setParseAction(pushUnary)
+factor = atom
+term = factor + ZeroOrMore(((MUL | DIV) + factor).setParseAction(pushFirst))
+arithmeticExp = term + ZeroOrMore(((ADD | SUB) + term).setParseAction(pushFirst))
 
 _comparisonExp = arithmeticExp + ZeroOrMore((LE | GE | LT | GT) + arithmeticExp)
 comparisonExp = _comparisonExp + ZeroOrMore((EQ | NE) + _comparisonExp)
--- Structures.py
+++ Structures.py
@@ -1,4 +1,3 @@
-
 from Grammar import *
 
 def formatAST(unformattedAST):
@@ -39,16 +38,16 @@ class Program(ASTNode):
     def parseFunctions(self):
         parseResult = function.scanString(self.content)
         for x in parseResult:
-            self.functionList.append(FunctionDeclare(self.content[x[1]:x[2]]))   
+            self.functionList.append(FunctionDeclare(self.content[x[1]:x[2]]))
         if not self.functionList:
             fail()
-            
+
 
 class FunctionDeclare(ASTNode):
     def __init__(self, content):
         super().__init__(content)
         self.parseFunction()
-    
+
     def parseFunction(self):
         parseResult = function.parseString(self.content)
         self.returnType = parseResult['returnType']
@@ -60,42 +59,74 @@ class FunctionDeclare(ASTNode):
         if not self.statementList:
             fail()
 
-    
+
 class Statement(ASTNode):
     def __init__(self, content):
         super().__init__(content)
+        self.expressionList = []
+        self.parseStatement()
 
+    def parseStatement(self):
+        scanResult = expression.scanString(self.content)
+        for x in scanResult:
+            if self.content[x[1]:x[2]] == "return ":
+                continue
+            self.expressionList.append(Expression(self.content[x[1]:x[2]]))
+        if not self.expressionList:
+            fail()
+
+
+class Expression(ASTNode):
+    def __init__(self, content):
+        super().__init__(content)
+        # self.parseExpression()
+
+    def parseExpression(self):
+        arithExpParseResult = arithmeticExp.parseString(self.content)
+        print(" ")
+
+    def parse_term(_term):
+        pass
 
 class UnaryOp(ASTNode):
     pass
 
 
 class BinOp(ASTNode):
-    pass
-
-
-# class Expression(ASTNode):
-#     def assignFields(self):
-#         self.num = self.tokens
-#         del self.tokens
-
-
-# class DataType(ASTNode):
-#     def assignFields(self):
-#         self.dataType = self.tokens
-#         del self.tokens
-
-
-# class Identifier(ASTNode):
-#     def assignFields(self):
-#         self.identifier = self.tokens
-#         del self.tokens
+    def __init__(self, content, op, left, right):
+        super().__init__(content)
+        self.op = op
+        self.left = left
+        self.right = right
 
 
 if __name__ == "__main__":
-    Path = "D:/DATA/Python_ws/CUDA_Parser/test/stage_4/valid/precedence_2.c"
-    P = Program(Path)
-    print(P.functionList[0].statementList[0].content)
-    # Path = "D:/DATA/Python_ws/CUDA_Parser/test/stage_4/valid/or_false.c"
-    # P = Program(Path)
-    # print(P.functionList[0].statementList[0].content)
+    dir = "D:/DATA/Python_ws/CUDA_Parser/test/stage_4/valid/"
+    def test(path):
+        P = Program(dir + path)
+        print(P.functionList[0].statementList[0].content)
+
+    # stage 4
+    test("and_false.c")
+    test("and_true.c")
+    test("eq_false.c")
+    test("eq_true.c")
+    test("ge_false.c")
+    test("ge_true.c")
+    test("gt_false.c")
+    test("gt_true.c")
+    test("le_false.c")
+    test("le_true.c")
+    test("lt_false.c")
+    test("lt_true.c")
+    test("ne_false.c")
+    test("ne_true.c")
+    test("or_false.c")
+    test("or_true.c")
+    test("precedence.c")
+    test("precedence_2.c")
+    test("precedence_3.c")
+    test("precedence_4.c")
+
+
+
