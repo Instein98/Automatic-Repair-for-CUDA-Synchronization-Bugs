caab40e56ba8c38940292bc3cc6912e62182bc37
Instein98 <instein98@gmail.com>
2019-06-26 19:59:14 +0800

    modified arithmetic expression

--- Grammar.py
+++ Grammar.py
@@ -15,28 +15,24 @@ LBrace, RBrace, LParen, RParen, semicolon = map(Suppress, "{}();")
 ADD, SUB, MUL, DIV = map(Literal, "+-*/")
 AND, OR, EQ, NE, LT, LE, GT, GE = map(Literal, ["&&", "||", "==", "!=", "<", "<=", ">", ">="])
 unaryOp = oneOf("! - ~")  # must have space separator
-# binOp = oneOf("+ - * /")
 number = Word(nums+'.')
 identifier = Word(alphas+'_', alphanums+'_')
 space = White(min=1)
 dataType = Literal("int")
 
-arithmeticExp = Forward()
+# Arithmetic expressions
+expression = Forward()
+
 factor = Forward()
-factor << (number | identifier | LParen + arithmeticExp + RParen | unaryOp + factor)
-_term = Forward()
-_term << Optional((MUL | DIV) + factor + _term)
-term = factor + _term
-_arithmeticExp = Forward()
-_arithmeticExp << Optional((ADD | SUB) + term + _arithmeticExp)
-arithmeticExp << term + _arithmeticExp
+factor << (number | identifier | LParen + expression + RParen | unaryOp + factor)
+term = factor + ZeroOrMore((MUL | DIV) + factor)
+arithmeticExp = term + ZeroOrMore((ADD | SUB) + term)
 
 _comparisonExp = arithmeticExp + ZeroOrMore((LE | GE | LT | GT) + arithmeticExp)
 comparisonExp = _comparisonExp + ZeroOrMore((EQ | NE) + _comparisonExp)
 _expression = comparisonExp + ZeroOrMore(AND + comparisonExp)
-expression = _expression + ZeroOrMore(OR + _expression)  # include the arithmeticExp and logicExp
+expression << _expression + ZeroOrMore(OR + _expression)  # include the arithExp and logicExp
 
-"""Failed precedence_2.c"""
 
 statement = Literal("return") + space + expression + semicolon
 function = dataType('returnType') + identifier('fnName') + LParen + RParen + LBrace + statement('statements') + RBrace
